b86c820e9efdb7321045a70399eee95297fa7163
anoopchaurasia <anoop26667@gmail.com>
2014-06-13 19:34:43 +0530

    fabric practo

---
+++ Practo/pages/hub/section1Page.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    "use strict";
+
+    var ControlConstructor = WinJS.UI.Pages.define("/pages/hub/section1Page.html", {
+        // This function is called after the page control contents 
+        // have been loaded, controls have been activated, and 
+        // the resulting elements have been parented to the DOM. 
+        ready: function (element, options) {
+            options = options || {};
+            var oldsearchparam;
+            $("input#speciality").keyup(function () {
+                var value = this.value.trim();
+                if (oldsearchparam === value || value === "") {
+                    return;
+                }
+                oldsearchparam = value;
+                seach(value);
+            });
+
+        },
+    });
+
+    function seach(key) {
+        $.ajax({
+            url: "https://www.practo.com/health/api/metrics",
+            data: { city_id: 0 },
+            success: function (data) { debugger },
+            error: function (data) { debugger },
+            header: {
+                "X-AUTH-TOKEN": window.auth_token,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+    }
+
+    // The following lines expose this control constructor as a global. 
+    // This lets you use the control as a declarative control inside the 
+    // data-win-control attribute. 
+
+    WinJS.Namespace.define("HubApps_SectionControls", {
+        Section1Control: ControlConstructor
+    });
+})();
